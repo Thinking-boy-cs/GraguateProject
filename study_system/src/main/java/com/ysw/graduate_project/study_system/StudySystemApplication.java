7dcc4972653a1fea02ef5a355e2895475cbfa79e
Yusw <1499356850@qq.com>
2022-04-05 21:54:12 +0800

    The Project start

---
+++ study_system/src/main/java/com/ysw/graduate_project/study_system/StudySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ysw.graduate_project.study_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySystemApplication.class, args);
+    }
+
+}
---
+++ study_system/src/test/java/com/ysw/graduate_project/study_system/StudySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysw.graduate_project.study_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
